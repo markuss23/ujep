fca9ee3ca20c931350d4e6a9e08f9ec004d4ac7b
marek.tremel <tremelmarek@gmail.com>
2024-10-10 12:05:15 +0200

    oonv

---
+++ oonv/Singleton01/Singleton01/Program.cs
@@ -0,0 +1,27 @@
+﻿class  Singleton01
+{
+    public static void Main(string[] args)
+    {
+        var s1 = Singleton.GetInstance();
+        var s2 = Singleton.GetInstance();
+        Console.WriteLine(s1 == s2 ? "asdasd" : "aaaaaa");
+    }
+}
+
+
+class Singleton
+{
+    private static Singleton? _instance;
+
+    private Singleton()
+    {
+    }
+
+    public static Singleton GetInstance()
+    {
+        return _instance ??= new Singleton();
+    }
+}
+
+
+
