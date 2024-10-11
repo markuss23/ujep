7b334ec3e0b984642fb983fbf3ac9debcb2cd799
marek.tremel <tremelmarek@gmail.com>
2024-10-11 17:50:26 +0200

    11.10.2024

---
+++ num/02.R
@@ -0,0 +1,31 @@
+n <- 40
+A <- matrix(runif(n*n, -1, 1), nrow=n)
+b <- runif(n, -1, 1)
+
+x <- numeric(n)
+
+# GAUSS bez vektoritace
+for(k in 1:10){
+  for( i in 1:n){
+    soucet <- 0
+    for (j in 1:n){
+      if(j!=i) soucet <- soucet + A[i,j]*x[j]
+    }
+    x[i] <- (b[i] - soucet)/A[i,i]
+  }
+  # print(x)
+}
+solve(A,b)
+
+# jacobiho s vektoirzaci
+for(k in 1:10){
+  for( i in 1:n){
+
+    x[i] <- (b[i] - sum(A[i,]* x)+A[i,i]*x[i])/A[i,i]
+  }
+  # print(x)
+}
+
+
+
+
---
+++ oonv/Builder/Builder/Program.cs
@@ -0,0 +1,124 @@
+﻿using System.Text;
+
+namespace Builder;
+interface IBuilder
+{
+    void BuildPartA();
+    void BuildPartB();
+    void BuildPartC();
+    
+    void Reset();
+    
+    StringBuilder GetProduct();
+}
+
+
+class Director
+{
+    private IBuilder _builder;
+
+    public Director(IBuilder builder)
+    {
+        _builder = builder;
+    }
+
+    public void ChangeBuilder(IBuilder builder)
+    {
+        _builder = builder;
+    }
+    
+    public StringBuilder Make(string type)
+    {
+        _builder.Reset();
+        switch (type)
+        {
+            case "Concrete":
+                _builder.BuildPartA();
+                break;
+            case "Malta":
+                _builder.BuildPartA();
+                _builder.BuildPartB();
+                _builder.BuildPartC();
+                break;
+            default:
+                _builder.BuildPartB();
+                break;
+        }
+       return _builder.GetProduct();
+    }
+}
+
+
+class ConcreteBuilder : IBuilder
+{
+    private StringBuilder _product = new StringBuilder();
+    
+    public void Reset()
+    {
+        _product.Clear();
+    }
+    public void BuildPartA()
+    {
+        _product.Append("A");
+    }
+
+    public void BuildPartB()
+    {
+        _product.Append("B");
+    }
+
+    public void BuildPartC()
+    {
+        _product.Append("C");
+    }
+    
+    public StringBuilder GetProduct()
+    {
+        return _product;
+    }
+}
+class MaltaBuilder : IBuilder
+{
+    private StringBuilder _product = new StringBuilder();
+    
+    public void Reset()
+    {
+        _product.Clear();
+    }
+    public void BuildPartA()
+    {
+        _product.Append("A Malta");
+    }
+
+    public void BuildPartB()
+    {
+        _product.Append("B Malta");
+    }
+
+    public void BuildPartC()
+    {
+        _product.Append("C Malta");
+    }
+    
+    public StringBuilder GetProduct()
+    {
+        return _product;
+    }
+}
+class Program {
+    static void Main( string[] args )
+    {
+        ConcreteBuilder b = new ConcreteBuilder();
+        MaltaBuilder b2 = new MaltaBuilder();
+        
+        Director d = new Director(b);
+        
+        System.Console.WriteLine(d.Make("Concrete"));
+        
+        d.ChangeBuilder(b2);
+        
+        System.Console.WriteLine(d.Make("Malta"));
+        
+        
+    }
+}
